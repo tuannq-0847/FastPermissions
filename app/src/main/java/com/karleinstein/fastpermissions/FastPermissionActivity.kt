653aee28af7c82b3ca34ad1657055706c50ea410
tuannguyen <tuannguyen@tuannguyens-MacBook-Pro.local>
2020-08-15 18:14:55 +0700

    Integrate Fast Permissions Handle

---
+++ app/src/main/java/com/karleinstein/fastpermissions/FastPermissionActivity.kt
@@ -0,0 +1,36 @@
+package com.karleinstein.fastpermissions
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+
+class FastPermissionActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        FastPermission.getPermissionActivity(this)
+        window.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        val deniedPermissions = mutableListOf<String>()
+        val deniedPermissionsForever = mutableListOf<String>()
+        permissions.forEachIndexed { i, permission ->
+            val isShouldShowRationale = shouldShowRequestPermissionRationale(permission)
+            when (grantResults[i]) {
+                PermissionChecker.PERMISSION_DENIED, PermissionChecker.PERMISSION_DENIED_APP_OP -> {
+                    if (!isShouldShowRationale) deniedPermissionsForever.add(permission)
+                    else deniedPermissions.add(permission)
+                }
+            }
+        }
+        FastPermission.getDeniedPermissions(deniedPermissions, deniedPermissionsForever)
+    }
+}
