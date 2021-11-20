4b89a911501a572ad8dc57278061db9034152644
tuannq-0847 <fckurmother770@gmail.com>
2021-11-20 15:41:14 +0700

    create ads intertitial

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 buildscript {
-    val hiltVersion = "2.33-beta"
-//    val kotlinVersion by extra("1.4.31")
+    val kotlinVersion by extra("1.5.31")
     repositories {
         google()
-        mavenCentral()
+        jcenter()
+        mavenLocal()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
